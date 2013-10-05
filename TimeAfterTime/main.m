316d280a1dedd3ccd5ef8c7bf05a708357441956
Arun Agrawal <arunagw@gmail.com>
2013-10-05 22:39:21 +0200

    Initial Commit

---
+++ TimeAfterTime/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  TimeAfterTime
+//
+//  Created by Arun Agrawal on 05/10/13.
+//  Copyright (c) 2013 Arun Agrawal. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+

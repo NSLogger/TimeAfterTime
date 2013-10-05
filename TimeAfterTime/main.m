ef1885711417cbb84fb86f59f5cbabb98fccc23e
Arun Agrawal <arunagw@gmail.com>
2013-10-06 01:27:55 +0200

    Few things try with DateClass

--- TimeAfterTime/main.m
+++ TimeAfterTime/main.m
@@ -12,11 +12,39 @@ int main(int argc, const char * argv[])
 {
 
     @autoreleasepool {
-        
-        // insert code here...
-        NSLog(@"Hello, World!");
-        
+
+        NSDateComponents *comps = [[NSDateComponents alloc] init];
+        [comps setYear:1985];
+        [comps setMonth:05];
+        [comps setDay:14];
+
+        NSCalendar *g = [[NSCalendar alloc] initWithCalendarIdentifier:NSGregorianCalendar];
+        NSDate *dateOfBirth = [g dateFromComponents:comps];
+
+        NSDate *laterDate = [NSDate date];
+
+        double seconds = [laterDate timeIntervalSinceDate:dateOfBirth];
+
+        NSLog(@"\nMy age is %.2f.", (((seconds/60)/60)/24)/360);
+
+
+        NSDate *now = [[NSDate alloc] init];
+
+        NSLog(@"\nnow is %@", now);
+
+        NSTimeZone *currentTimeZone = [NSTimeZone systemTimeZone];
+
+        BOOL dayLightSaving = [currentTimeZone isDaylightSavingTime];
+
+        if(dayLightSaving){
+            NSLog(@"Yes timezone is daylightsaving");
+        }
+
+        else {
+            NSLog(@"NO timezone is no daylightsaving");
+        }
+
+
     }
     return 0;
 }
-
